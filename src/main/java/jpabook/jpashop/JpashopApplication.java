b9d117b60373c3bd23c00fb2ecbadddc113e9387
dongyeong <dge04200@naver.com>
2023-02-23 17:26:56 +0900

    2023-02-23 spring boot start

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -6,7 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpashopApplication {
 
+
 	public static void main(String[] args) {
+
+		Hello hello = new Hello();
+		hello.setData("Hello");
+		String data = hello.getData();
+		System.out.println("data=" + data);
+
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
